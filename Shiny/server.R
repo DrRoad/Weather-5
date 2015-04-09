bd87826e8b8cddafe90d37ef9c7c561dd276a53b
rivkahcarl <rivkah@noodle.org>
2015-04-09 09:31:41 -0400

    Begins validation, still needs logic around strings

--- Shiny/server.R
+++ Shiny/server.R
@@ -18,25 +18,30 @@ shinyServer(function(input, output) {
 					 "<h4>**Scarf</h4>",
 					 "<h4>**Gloves</h4>", 
 					 "<h4>**Hat</h4>", sep="<br/>"))
-				} else if (40 < input$temperature & input$temperature < 55){
+				} else if (40 <= input$temperature & input$temperature < 55){
 					HTML(paste("<h2>Today's clothing should include:</h2>",
 					 "<h4>**Tights or Leggings</h4>",
 					 "<h4>**Long Sleeves</h4>",
 					 "<h4>**Winter Coat</h4>", sep="<br/>"))
-				} else if (56 < input$temperature & input$temperature < 65){
+				} else if (56 <= input$temperature & input$temperature < 65){
 					HTML(paste("<h2>Today's clothing should include:</h2>",
 					 "<h4>**Leggings</h4>", 
 					 "<h4>**Long Sleeves</h4>", 
 					 "<h4>**Lightweight Coat</h4>", sep="<br/>"))
-				} else if (66 < input$temperature & input$temperature < 77){
+				} else if (66 <= input$temperature & input$temperature < 79){
 					HTML(paste("<h2>Today's clothing should include:</h2>",
 						"<h4>**Long Sleeves</h4>", 
 						"<h4>**Sweater</h4>", sep="<br/>"))
-				} else {
+				} else if (80 <= input$temperature & input$temperature <= 110){
 					HTML(paste("<h2>Today's clothing should include:</h2>",
 					 "<h4>**Short Sleeves</h4>",
 					 "<h4>YAY! It is FINALLY summer</h4>", sep="<br/>"))
 				}
+		} else if (is.na(input$temperature)){
+			HTML("")
+		}
+		else if (!is.numeric(input$temperature) & !is.na(input$temperature)){
+			HTML("Please put in a correct number for the temperature")
 		}
 	})
 })
